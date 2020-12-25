adddc5c3f29c40585cac7d0c3d76ac2e2582437e
jinsujj <wlstncjs1234@naver.com>
2020-12-25 21:59:34 +0900

    .NET 5 Update

--- MyApp/Data/Repositorys/DotNetNote/NoteCommentRepository.cs
+++ MyApp/Data/Repositorys/DotNetNote/NoteCommentRepository.cs
@@ -1,11 +1,9 @@
 ﻿using Dapper;
 using MyApp.Models.DotNetNote;
 using MySql.Data.MySqlClient;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyApp.Data.Repositorys.DotNetNote
 {
