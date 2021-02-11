5643522268f5bfd353a2706675971a77b706f0ce
Jinsu <wlstncjs1234@naver.com>
2021-02-12 02:16:44 +0900

    Revert "Revert "Custom Query""
    This reverts commit d03a910ffbe5ee54f20c21097a90c8cee51132a7.

---
+++ MyApp/Models/Custom.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models
+{
+    public class Custom
+    {
+        public string buffquery { get; set; }
+        private string query;
+        private List<string> columnList;
+        private List<List<string>> dataRaw;
+
+        public Custom()
+        {
+            this.columnList = new List<string>();
+            this.dataRaw = new List<List<string>>();
+        }
+
+        public string getQuery()
+        {
+            return query;
+        }
+
+        public void setQuery(string str)
+        {
+            query = str;
+        }
+
+        public void setColumn(string str)
+        {
+            columnList.Add(str);
+        }
+        public List<string> getColumnList()
+        {
+            return columnList;
+        }
+
+        public void setRaw(List<String> listStr)
+        {
+            // 동일한 값 주소를 Add 하면 마지막 Add 값으로 전부 복사되어 변경
+            List<string> buff = listStr;
+            dataRaw.Add(buff.ToList());
+        }
+        
+        public List<List<string>> getRaw()
+        {
+            return dataRaw;
+        }
+        
+    }
+}
