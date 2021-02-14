fd5bae17308a1fb81b366bc7de9c3e22e122e710
Jinsu <wlstncjs1234@naver.com>
2021-02-14 21:03:29 +0900

    Update BlogController.cs

--- MyApp/Controllers/BlogController.cs
+++ MyApp/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MyApp.Data.Repositorys.DashBoard;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,19 @@ namespace MyApp.Controllers
     {
         private readonly IConfiguration _config;
         private readonly ILogger<BlogController> _logger;
+        private readonly IDashBoardRepository _repository;
 
-        public BlogController(IConfiguration config, ILogger<BlogController> logger)
+        public BlogController(IConfiguration config, ILogger<BlogController> logger, IDashBoardRepository repository)
         {
             _config = config;
             _logger = logger;
+            _repository = repository;
         }
 
         public IActionResult Index()
         {
             _logger.LogInformation("Blog -> Home 페이지 로딩");
+            _repository.Log("Blog", HttpContext.Connection.RemoteIpAddress.ToString());
             return Redirect("../DotNetNote/IndexCard");
         }
 
