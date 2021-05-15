250d3666c8db1abc7972a14d268edbd9b8e34777
Jinsujj <wlstncjs1234@naver.com>
2021-05-15 23:25:40 +0900

    Create ResultLog.cs

---
+++ MyApp/Models/SexualTest/ResultLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApp.Models.SexualTest
+{
+    public class ResultLog
+    {
+        public string ip { get; set; }
+        public string date { get; set; }
+        public int age { get; set; }
+        public string sex { get; set; }
+        public string type { get; set; }
+        public string[] num { get; set; } = new string[13];
+    }
+}
